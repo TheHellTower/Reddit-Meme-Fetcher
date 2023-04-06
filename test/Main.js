feb40d44d3fc60bd3199b1c1af02788f9fc37fb7
TheHellTower <thehelltower@tuta.io>
2023-04-06 19:06:53 +0200

    Final First Version

--- test/Main.js
+++ test/Main.js
@@ -1,30 +1,47 @@
-const RedditMemesFetcherClient = new (require('../src/RedditMemesFetcher.js'))("json");
+const RedditMemesFetcherClient = new (require("reddit-meme-fetcher"))("json");
 
+
+//Example JSON output:
 /*
-Optional parameters(in order):
-    Type: "today" and "random" (default: random)
-    Format "text" and "json" (default: text)
-
-Example JSON output:
-    {
-        text: 'A pig`s orgasm lasts for 30 minutes.',
-        source: 'djtech.net',
-        source_url: 'http://www.djtech.net/humor/useless_facts.htm',
-        language: 'en',
-        permalink: 'https://uselessfacts.jsph.pl/api/v2/facts/20703986d294054ff282e1ee212e3242'
-    }
-
-    Usage: console.log(res["text"]);
-Example Text output:
-    "A pig`s orgasm lasts for 30 minutes."
-
-    Usage: console.log(res);
-
-Example getUselessFact Usage:
-1) await UselessFactsClient.getUselessFact("random", "json");
-2) UselessFactsClient.getUselessFact("random", "json").then(res => {
-    console.log(res["text"]);
-})
+{
+   "error":false,
+   "data":{
+      "sub":{
+         "name":"Memes",
+         "subscribers":25075037
+      },
+      "author":{
+         "name":"GDino12",
+         "fullName":"t2_anbdckfn",
+         "flairText":null,
+         "isBlocked":false,
+         "isPremium":false
+      },
+      "post":{
+         "title":"Derp snake",
+         "imageURL":{
+            "url":"https://i.redd.it/wg115d8oq9sa1.jpg",
+            "urlOverriden":"https://i.redd.it/wg115d8oq9sa1.jpg"
+         },
+         "thumbnail":"https://b.thumbs.redditmedia.com/8KdasQHACh-BXrFz4Ro-ZBLFuj-5WtuVAQGXldqGFCU.jpg",
+         "votes":{
+            "up":684,
+            "upRatio":0.95,
+            "Down":0
+         },
+         "score":684,
+         "comments":19,
+         "viewCount":0,
+         "permaLink":"https://www.reddit.com/r/memes/comments/12ddsib/derp_snake/",
+         "extra":{
+            "archived":false,
+            "pinned":false,
+            "over18":false,
+            "createdUTC":1680770850
+         }
+      }
+   }
+}
 */
 
 RedditMemesFetcherClient.getRandomMeme("json").then(res => {
